ed0ba5ebfcb546f318a64b70767dc1b04557cbda
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2025-08-08 11:47:06 +0530

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     # For development, the reloader is useful.
     # For production, use Gunicorn as specified in the Dockerfile.
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
